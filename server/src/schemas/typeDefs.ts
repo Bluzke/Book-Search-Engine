6d69986f0f7447d8870f377ed0334e76cadc4733
Justin Silber <jsilber60@gmail.com>
2024-12-05 14:39:55 -0500

    added typeDefs code

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,55 @@
+const typeDefs = `
+
+# User Type
+type User {
+_id: ID
+username: String
+email: String
+bookCount: Int
+savedBooks: [Book]!
+}
+
+# Book Type
+type Book {
+bookId: ID
+authors: [String]!
+description: String
+title: String
+image: String
+link: String
+}
+
+# Auth Type
+type Auth {
+token: String
+user: User
+}
+
+# Query Type
+type Query {
+me: User
+}
+
+# BookInput for saveBook
+input BookInput {
+bookId: ID!
+authors: [String]!
+description: String!
+title: String!
+image: String
+link: String
+
+}
+
+
+# Mutation Type
+type Mutation {
+login(email: String!, password: String!): Auth
+addUser(username: String!, email: String!, password: String!): Auth
+saveBook(bookInput: BookInput!): User
+removeBook(bookId: ID!): User
+}
+
+`;
+
+export default typeDefs;
